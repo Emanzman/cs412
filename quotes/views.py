f77a2dde60ef7dc6b7f0ab7bd2fa1c9916a78ac8
Emmanuel Eyob <emanzman@bu.edu>
2025-05-20 12:02:32 -0400

    Created quotes and images list in views

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+quotes_list = ["Darkness cannot drive out darkness, only light can do that. Hate cannot drive out hate, only love can do that.",
+               "The ultimate measure of a man is not where he stands in moments of comfort and convenience, but where he stands at times of challenge and controversy.",
+               "True peace is not merely the absence of tension; it is the presence of justice."
+]
+
+images_list = ["https://www.uncsa.edu/chancellor/communications/img/martin-luther-king-jr.jpg",
+               "https://bunny-wp-pullzone-5vqgtgkbhi.b-cdn.net/wp-content/uploads/2023/04/Civil-Rights-Leaders-in-Selma.jpg",
+               "https://hips.hearstapps.com/hmg-prod/images/civil-rights-leader-martin-luther-king-waves-to-supporters-news-photo-1704903196.jpg"
+
+]
