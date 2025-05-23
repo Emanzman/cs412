64d347d1b87c8c477a6de67aa7c97bb6017dd1d8
Emmanuel Eyob <emanzman@bu.edu>
2025-05-23 18:49:07 -0400

    Added code to views

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,3 +3,20 @@ import random
 
 # Create your views here.
 
+def main(request):
+    template_name= 'restaurant/main.html'
+    return render(request, template_name)
+
+def order(request):
+    template_name = 'restaurant/order.html'
+    menu = [
+        "Kitfo",
+        "Tibs",
+        "Doro Wat",
+        "Beyeaynetu",
+
+    ]
+
+    daily_special = random.choice(menu)
+    return render(request, template_name, {'daily_special': daily_special})
+
