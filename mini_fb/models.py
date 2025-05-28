91c98ee1f454bc1abaed3146c9e5ce129d63ea18
Emmanuel Eyob <emanzman@bu.edu>
2025-05-28 15:32:57 -0400

    Changed model and now display status messages on profile.

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,6 +10,19 @@ class Profile(models.Model):
   city = models.CharField(blank=False)
   email= models.EmailField(blank=False)
   profile_image_url = models.URLField(blank=False)
+
+  def get_status_messages(self):
+    return self.statusmessage_set.all().order_by('-timestamp')
+
+
+class StatusMessage(models.Model):
+  timestamp = models.DateTimeField(default=timezone.now)
+  message = models.TextField()
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.message} {self.timestamp.strftime('%B %d %Y, %I:%M %p')}."  
+
 
 
 
